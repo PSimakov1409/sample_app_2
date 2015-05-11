7f35ed7852f63236b57fca1bbbb8b9805eb673b2
PSimakov1409 <PSimakov1409@gmail.com>
2015-05-11 20:23:20 +0300

    Add About page and change Help page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  # Описываем Home страницу
+  describe "Home page" do
+    # При посещении Home страницы по адресу /static_pages/home, 
+    # контент должен содержать слова “Sample App”
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      # Описание ожидания того, что данная страница должна 
+      # содержать правильный контент.
+      expect(page).to have_content('Sample App')
+    end
+  end
+  # Описываем Help страницу
+  describe "Help page" do
+    it "should have yhe content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+  #Описываем About страницу
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+end
