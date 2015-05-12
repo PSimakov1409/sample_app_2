93e7e833996bc61c47b5d97638f5a6613999d293
PSimakov1409 <PSimakov1409@gmail.com>
2015-05-12 17:32:10 +0300

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,11 @@ describe "Static pages" do
       # содержать правильный контент.
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      # Проверяет, что внутри тега title содержится "Ruby on Rails Tutorial Sample App | Home"
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
   # Описываем Help страницу
   describe "Help page" do
@@ -18,12 +23,20 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
   #Описываем About страницу
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
