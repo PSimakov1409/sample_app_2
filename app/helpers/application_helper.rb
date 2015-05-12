35f11538b4375159e6408524791799dea9157b5f
PSimakov1409 <PSimakov1409@gmail.com>
2015-05-12 20:51:30 +0300

    Some comment for helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  #Возращение полного заголовка на странице
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
+  # Возвращает полный заголовок зависящий от страницы
+  def full_title(page_title)                          # Определение метода
+    base_title = "Ruby on Rails Tutorial Sample App"  # Назначение переменной
+    if page_title.empty?                              # Булевый тест
+      base_title                                      # Явное возвращение
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title}"                 # Интерполяция строки
     end
   end
 end
