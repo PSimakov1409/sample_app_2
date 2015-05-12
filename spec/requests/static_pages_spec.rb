e3116e57908b43cf2c5addb064bcb873550f8d34
PSimakov1409 <PSimakov1409@gmail.com>
2015-05-12 17:50:27 +0300

    Add Contact page, add func 'let' for spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  #функцию let которая создает переменную соответствующую ее аргументу
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   # Описываем Home страницу
   describe "Home page" do
     # При посещении Home страницы по адресу /static_pages/home, 
@@ -14,7 +16,8 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       # Проверяет, что внутри тега title содержится "Ruby on Rails Tutorial Sample App | Home"
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      # "#{base_title}" = Ruby on Rails Tutorial Sample App
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   # Описываем Help страницу
@@ -25,7 +28,7 @@ describe "Static pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   #Описываем About страницу
@@ -36,7 +39,18 @@ describe "Static pages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+  #Описываем Contact страницу
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
