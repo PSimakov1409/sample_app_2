1c24f06968f6fd1467d6cde8b0ba29dc8471e2a3
PSimakov1409 <PSimakov1409@gmail.com>
2015-05-12 19:44:42 +0300

    Add helper full_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,11 +13,15 @@ describe "Static pages" do
       # содержать правильный контент.
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       # Проверяет, что внутри тега title содержится "Ruby on Rails Tutorial Sample App | Home"
       # "#{base_title}" = Ruby on Rails Tutorial Sample App
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
   # Описываем Help страницу
